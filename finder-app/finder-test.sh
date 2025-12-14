1beb45fbcd34afb71ef6990d0e47bd340cbeb755
Marco Castellari <marco.castellari@hotmail.it>
2025-12-14 16:04:38 +0100

    assignment 2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -59,6 +59,30 @@ done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
+# # remove temporary directories
+# rm -rf /tmp/aeld-data
+# 
+# set +e
+# echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+# if [ $? -eq 0 ]; then
+# 	echo "success"
+# 	exit 0
+# else
+# 	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+# 	exit 1
+# fi
+
+# Assignment 2
+make clean
+make writer
+
+for i in $( seq 1 $NUMFILES)
+do
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+done
+
+OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
